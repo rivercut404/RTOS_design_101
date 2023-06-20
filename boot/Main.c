47fe3a82ebfe0903a56a4bdc23c1c6b6d3354b5e
rivercut404 <rivercut404@gmail.com>
2023-06-20 21:41:48 +0900

    RTOS without Kernel API for yield

---
+++ boot/Main.c
@@ -0,0 +1,102 @@
+#include "stdint.h"
+#include "HalUart.h"
+#include "stdbool.h"
+#include "HalTimer.h"
+#include "task.h"
+
+#include "HalUart.h"
+#include "HalInterrupt.h"
+#include "stdio.h"
+#include "stdlib.h"
+
+static void Hw_init(void);
+static void Printf_test(void);
+static void Timer_test(void);
+static void Kernel_init(void);
+
+// User task function declaration 
+void User_task0(void);
+void User_task1(void);
+void User_task2(void);
+
+void main(void) {
+    Hw_init();
+
+    uint32_t i = 100;
+    while (i--) {
+        Hal_uart_put_char('N');
+    }
+    Hal_uart_put_char('\n');
+
+    putstr("Hello World!\n");
+
+    Printf_test();
+
+	Timer_test();
+
+    while (true);
+}
+
+static void Hw_init(void) {
+    Hal_interrupt_init();
+    Hal_uart_init();
+}
+
+static void Printf_test(void) {
+    char *str = "printf pointer test";
+    char *nullptr = 0;
+    uint32_t i = 5;
+    uint32_t *sysctrl0 = (uint32_t*)0x10001000;
+
+    debug_printf("%s\n", "Hello printf");
+    debug_printf("output string pointer: %s\n", str);
+    debug_printf("%s is null pointer, %u number\n", nullptr, 10);
+    debug_printf("%u = 5\n", i);
+    debug_printf("dec=%u hex=%x\n", 0xff, 0xff);
+    debug_printf("print zero %u\n", 0);
+
+	debug_printf("SYSCTRL0: %x\n", *sysctrl0);
+}
+
+static void Timer_test(void) {
+    while (true) {
+	    debug_printf("current count: %u\n", Hal_timer_get_1ms_counter());
+		delay(1000);
+	}
+}
+
+static void Kernel_init(void) {
+    uint32_t taskId;
+
+	Kernel_task_init();
+
+	taskId = Kernel_task_create(User_task0);
+	if (NOT_ENOUGH_TASK_NUM == taskId)
+	    putstr("Task0 creation fail\n");
+
+	taskId = Kernel_task_create(User_task1);
+	if (NOT_ENOUGH_TASK_NUM == taskId)
+	    putstr("Task1 creation fail\n");
+
+    taskId = Kernel_task_create(User_task2);
+	if (NOT_ENOUGH_TASK_NUM == taskId)
+	    putstr("Task2 creation fail\n");
+}
+
+void User_task0(void) {
+    debug_printf("User task #0\n");
+
+	while (true);
+}
+
+void User_task1(void) {
+    debug_printf("User task #1\n");
+
+	while (true);
+}
+
+void User_task2(void) {
+    debug_printf("User task #2\n");
+
+	while (true);
+}
---
+++ lib/stdlib.h
@@ -0,0 +1,6 @@
+#ifndef _LIB_STDLIB_H
+#define _LIB_STDLIB_H    
+
+void delay(uint32_t ms);
+
+#endif /* LIB_STDLIB_H */
